12ab358f574105b72381d76b0fb2518018b9ca4d
rodrigocastelo <rodrigocastelo85@gmail.com>
2024-10-08 12:15:19 -0300

    Create commit initial

---
+++ script.js
@@ -0,0 +1,16 @@
+let idade = 61;
+let nome = "Rodrigo";
+
+
+    if (idade > 18 && idade < 60) {
+        console.log(`${nome} é adulto`);
+    }
+    else if (idade >= 60) {
+        console.log (`${nome} é idoso`);
+    }
+
+    else {
+        console.log (`${nome} é adolescente`);
+    }
+
+
